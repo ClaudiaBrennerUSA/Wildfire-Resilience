a3823a940b5f4004035c9d199fb7aac5f8798900
James Johnson <jjohnson@ip-172-31-44-213.us-east-2.compute.internal>
2023-11-09 23:10:05 +0000

    add new temporary pages add new styling class for paragraph header move top navigation menu to a common function

--- src/main/index.js
+++ src/main/index.js
@@ -10,6 +10,32 @@ const home = (req, res) =>
     res.sendFile(path.join(__dirname, 'views/index.html'))
 }
 
+const usingTheScorecard = (req, res) =>
+{
+    console.log("HELLO from using");
+    res.sendFile(path.join(__dirname, 'views/using_the_scorecard.html'))
+}
+
+const faq = (req, res) =>
+{
+    res.sendFile(path.join(__dirname, 'views/faq.html'))
+}
+
+const news = (req, res) =>
+{
+    res.sendFile(path.join(__dirname, 'views/press_release.html'))
+}
+
+const scorecardInfo = (req, res) =>
+{
+    res.sendFile(path.join(__dirname, 'views/scorecard_info.html'))
+}
+
+const aboutUs = (req, res) =>
+{
+    res.sendFile(path.join(__dirname, 'views/about_us.html'))
+}
+
 
 const listenCallBackFunction = () =>
 {
@@ -17,6 +43,13 @@ const listenCallBackFunction = () =>
 }        
 
 app.get('/', home);
+app.get('/using_the_scorecard', usingTheScorecard);
+app.get('/faq', faq);
+app.get('/press_release', news);
+app.get('/news', news);
+app.get('/scorecard_info', scorecardInfo);
+app.get('/about_us', aboutUs);
+
 app.listen(PORT, listenCallBackFunction);
 app.use(express.static('static_files'));
 
---
+++ src/main/static_files/assets/javascript/topnav.js
@@ -0,0 +1,70 @@
+let __generateTopNav = (topNavContainerElementId) =>
+{
+    let topNavContainer= document.getElementById(topNavContainerElementId);
+    let markup = `
+    <nav class="navbar navbar-expand-sm navbar-dark custom-navbar-bg py-0">
+        <div class="nav1" id="top-navbar">
+            <div class="first-navbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a href="/" class="nav-link">HOME</a></li>
+                    <li><a href="/faq" class="nav-link">FAQ</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+  `;
+  topNavContainer.innerHTML = markup;
+}
+
+
+let generateTopNav = (topNavContainerElementId) =>
+{
+  let topNavContainer= document.getElementById(topNavContainerElementId);
+  let markup = `
+  <nav class="navbar navbar-expand-sm navbar-dark custom-navbar-bg py-0" >
+      <button
+        type="button"
+        class="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#topmenu-navbar"
+        aria-controls="topmenu-navbar"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="nav1" id="top-navbar">
+        <!-- <div class="cwpc">CWPC</div> -->
+        <div class="first-navbar">
+          <div class="collapse navbar-collapse" id="topmenu-navbar">
+            <!--navbar-navigation-->
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a href="/" class="nav-link active"> Home </a>
+              </li>
+              <li class="nav-item" >
+                <a href="/using_the_scorecard" class="nav-link"  style="margin-top: .5em; line-height: 1.2em; color: whitesmoke;" >Using the Scorecard</a>
+              </li>
+              <li class="nav-item">
+                <a href="/faq" class="nav-link"> FAQ </a>
+              </li>
+              <li class="nav-item">
+                <a href="/press_release" class="nav-link"> Press Release </a>
+              </li>                    
+              <li class="nav-item">
+                <a href="/scorecard_info" class="nav-link"> Scorecard Info </a>
+              </li>
+              <li class="nav-item">
+                <a href="/about_us" class="nav-link"> About Us </a>
+              </li>
+            </ul>
+          </div>
+          <!--/navbar-navigation-->
+        </div>
+      </div>
+    </nav>
+  `;
+  topNavContainer.innerHTML = markup;
+}
