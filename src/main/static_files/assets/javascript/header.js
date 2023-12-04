015c8a7077acece0c2d32301915b5d24c84a8e70
sadiya-b <bhawania.s@northeastern.edu>
2023-12-04 13:41:00 -0800

    Image updates

--- src/main/static_files/assets/javascript/header.js
+++ src/main/static_files/assets/javascript/header.js
@@ -52,11 +52,18 @@ let generateHeader = (headerContainerElementId) =>
   let markup = `
   <header>
     <div class="images-column">
-      <img src="assets/images/wildfire3logos2.png"
-        alt="CrowdDoing">
+      <!--<img src="assets/images/wildfire3logos2.png"
+        alt="CrowdDoing">-->
+        <div>
+      <img src="assets/images/cd-logo-white.png" alt="CDlogo" class="top-left-image">
+      <img src="assets/images/CWPC-white.png" alt="CWPC" class="top-right-image">
+      </div>
+      <div>
+      <img src="assets/images/arise.png" alt="arise" class="centered-image">
+      </div>
     </div>
     <div class="content-column">
-    <button id="hamburger" aria-expanded="false" onclick="toggleHamburger()">
+      <button id="hamburger" aria-expanded="false" onclick="toggleHamburger()">
        <i class="fa-solid fa-bars" aria-hidden="true"></i>
       </button>
       <nav class="global-navbar">
