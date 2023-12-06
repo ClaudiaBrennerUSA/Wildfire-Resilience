17e7226a73f6931d43d212b88841c248d9c6c771
sadiya-b <bhawania.s@northeastern.edu>
2023-12-06 14:39:54 -0800

    Updates to all other pages

--- src/main/views/forms/contact_form.js
+++ src/main/views/forms/contact_form.js
@@ -28,91 +28,81 @@ let generateContactUsForm = (req, res) =>
 
                 <!-- Bootstrap CSS -->
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-                    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-
+                    integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"
                 <!-- local styling -->
                 <link rel="stylesheet" type="text/css" href="../assets/style/style.css" />
-                <link rel="stylesheet" type="text/css" href="../assets/style/system.css" />
                 <link rel="stylesheet" type="text/css" href="/assets/style/system_forms.css">                
-
-
                 <title>Contact Us</title>
             </head>
             <body>
-
-                <div class="row-container" id="header-row" name="header-row"></div> <!--Header Populated By Javascript function -->
-                <!--<div class="row-container" id="top-nav-container" name="top-nav-container"></div> --><!--Top Navigation conatiner - to be replace with Sadiya's Header Populated By Javascript function -->
-
-
-                    <div class="w-100 text-center hero-text-level-0 ">
-                        <h1>Contact Us</h1>
-                    </div>
-
-                    <div class="container">
-
-                        <div class="card w-100 p-3">
-
-                            <form class="needs-validation"   id="contact-us-form" name="contact-us-form" automation-id="contact-us-form"  action="" method="post" novalidate>
-                            <div class="reverse-highlight w-100" style="display: none; vertical-align: top;"><label for='_id' style="width: 8em;">_id (hide me):</label><input type="text" class="form-control reverse-highlight" _id="id" name="_id" automation-id="_id" value="${(values['_id'] || -1)}"/><br/></div>
-
-
-                                <div class="flex-validated-form-element-container mt-2">
-                                    <label for="first-name">First Name</label>
-                                    <input type="text" class="form-control" id="first-name" name="first-name" value="${values['first-name']}" required />
-                                    <div class="invalid-feedback">First Name is Required</div>
-                                </div>
-        
-                                <div class="flex-validated-form-element-container mt-2">
-                                    <label for="last-name">Last Name</label>
-                                    <input type="text" class="form-control" id="last-name" name="last-name" value="${values['last-name']}" required />
-                                    <div class="invalid-feedback">Last Name is Required</div>
-                                </div>
-        
-                                
-                                <div class="flex-validated-form-element-container">
-                                <label for='email' style="width: 8em;">email:</label>
-                                <input type="email" class="form-control" id="sender-email" name="sender-email" automation-id="sender-email" value="${values['sender-email']}" required />
-                                <!-- <div class="flex-force-break"></div> -->
-                                <div class="invalid-feedback mb-3">A valid emaill address is Required</div>
-                                </div>
-                                
-                                
-                                
-                                <div class="flex-validated-form-element-container"><label for='organization' style="width: 8em;">Organization:</label><input type="text" class="form-control" id="organization" name="organization" automation-id="organization" value="${values['organization']}"/><br/></div>
-                                <div class="flex-validated-form-element-container"><label for='title'style="width: 8em;">Title:</label><input type="text" class="form-control" id="title" name="title" automatiion-id="title"  value="${values['title']}"/></div>
-                                
-                                
-                                
-                                <div class="flex-validated-form-element-container">
-                                <label for='postal-code'style="width: 8em;">Zip/Postal Code:</label>
+                <div id="header-row" name="header-row"></div>
+                <div class="contact-form-title">
+                    <h1>Contact Us</h1>
+                </div>
+                <div class="container">
+                    <div class="card w-100 p-3">
+                        <form class="needs-validation"   id="contact-us-form" name="contact-us-form" automation-id="contact-us-form"  action="" method="post" novalidate>
+                        <div class="reverse-highlight w-100" style="display: none; vertical-align: top;"><label for='_id' style="width: 8em;">_id (hide me):</label><input type="text" class="form-control reverse-highlight" _id="id" name="_id" automation-id="_id" value="${(values['_id'] || -1)}"/><br/></div>
+                            <div class="flex-validated-form-element-container mt-2">
+                                <label for="first-name">First Name:</label>
+                                <input type="text" class="form-control" id="first-name" name="first-name" value="${values['first-name']}" required />
+                                <div class="invalid-feedback">First Name is Required</div>
+                            </div>
+                            <div class="flex-validated-form-element-container mt-2">
+                                <label for="last-name">Last Name:</label>
+                                <input type="text" class="form-control" id="last-name" name="last-name" value="${values['last-name']}" required />
+                                <div class="invalid-feedback">Last Name is Required</div>
+                            </div>
+    
+                            
+                            <div class="flex-validated-form-element-container">
+                            <label for='email'>Email:</label>
+                            <input type="email" class="form-control" id="sender-email" name="sender-email" automation-id="sender-email" value="${values['sender-email']}" required />
+                            <!-- <div class="flex-force-break"></div> -->
+                            <div class="invalid-feedback mb-3">A valid emaill address is Required</div>
+                            </div>
+                            
+                            <div class="flex-validated-form-element-container">
+                                <label for='organization' >Organization:</label>
+                                <input type="text" class="form-control" id="organization" name="organization" automation-id="organization" value="${values['organization']}"/>
+                                <br/>
+                            </div>
+                            
+                            <div class="flex-validated-form-element-container">
+                                <label for='title'>Title:</label>
+                                <input type="text" class="form-control" id="title" name="title" automatiion-id="title"  value="${values['title']}"/>
+                            </div>
+                            
+                            
+                            
+                            <div class="flex-validated-form-element-container">
+                                <label for='postal-code'>Zip/Postal Code:</label>
                                 <input type="text" class="form-control" id="postal-code" name="postal-code" automation-id="postal-code"  value="${values['postal-code']}" required />
                                 <div class="invalid-feedback mb-3">Zipcode / Postal Code is Required></div>
-                                </div>
-                                
-                                
-                                <div class="flex-validated-form-element-container"  style="text-align: left;">
-                                    <label for='content' style="width: 6.5em;">Message:</label>
-                                    <textarea class="form-control" id="content" name="content" automation-id="content" rows="10" cols="40">${values.content}</textarea>
-                                </div>
+                            </div>
+                            
+                            
+                            <div class="flex-validated-form-element-container"  style="text-align: left;">
+                                <label for='content'>Message:</label>
+                                <textarea class="form-control" id="content" name="content" automation-id="content" rows="5" cols="40">${values.content}</textarea>
+                            </div>
 
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input force-1em" id="subscribe"  name="subscribe" automation-id="subscribe" value="${values['subscribe']}" __onclick="checkme(this)" ${values['subscribe']}>
-                                    <label class="form-check-label" for="subscribe">Subscribe</label>
-                                </div>
-                                <div class="form-check">
-                                    <input type="checkbox" class="form-check-input force-1em" id="requests-pilot" name="requests-pilot" automation-id="requests-pilot"  value="${values['requests-pilot']}"  __onclick="checkme(this)"   ${values['requests-pilot']}>
-                                    <label class="form-check-label" for="requests-pilot">I am interested in piloting with CWP&C</label>
-                                </div>
-                                
-                                <br/>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input force-1em" id="subscribe"  name="subscribe" automation-id="subscribe" value="${values['subscribe']}" __onclick="checkme(this)" ${values['subscribe']}>
+                                <label class="form-check-label" for="subscribe">Subscribe</label>
+                            </div>
 
-                                <input type="submit" id="'submitRequest" name="submitRequest" automation-id="sumitRequest" value="Submit Request">
-                            </form>
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input force-1em" id="requests-pilot" name="requests-pilot" automation-id="requests-pilot"  value="${values['requests-pilot']}"  __onclick="checkme(this)"   ${values['requests-pilot']}>
+                                <label class="form-check-label" for="requests-pilot">I am interested in piloting with CWP&C</label>
+                            </div>
+                            <br/>
+                            <input class="contact-card-button" type="submit" id="'submitRequest" name="submitRequest" automation-id="sumitRequest" value="Submit Request">
+                        </form>
 
-                        </div closes="card"> 
+                    </div closes="card"> 
 
-                    </div> <!-- /container -->
+                </div> <!-- /container -->
 
                 <div id="footer-container"></div>  <!-- /footer-container -->
 
