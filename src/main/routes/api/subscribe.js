d564226d761a6bddb26eaf02e64d1fe543febe0c
Sadiya Bhawania <86375403+sadiya-b@users.noreply.github.com>
2024-01-05 13:11:24 -0800

    Update subscribe.js
    removing config/env

--- src/main/routes/api/subscribe.js
+++ src/main/routes/api/subscribe.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const path = require ('path');
-const PORT = require('../../config/env').PORT;
 const bodyParser = require('body-parser');
 
 const app = express();
