09cc19295cc544f9d3f6d8a7d85d7ee63acf3ad9
Sadiya Bhawania <86375403+sadiya-b@users.noreply.github.com>
2024-01-29 11:52:02 -0800

    Route update for home page (#22)
    * changing default page route

--- src/main/static_files/assets/javascript/header.js
+++ src/main/static_files/assets/javascript/header.js
@@ -5,11 +5,11 @@ let generateHeader = (headerContainerElementId) =>
   <header>
     <div class="images-column">
       <div>
-       <a href="/home">  <img src="assets/images/cd-logo.png" alt="CDlogo" class="top-left-image"> </a>
-       <a href="/home">   <img src="assets/images/CWPCLogoW.png" alt="CWPC" class="top-right-image"> </a>
+       <a href="/">  <img src="assets/images/cd-logo.png" alt="CDlogo" class="top-left-image"> </a>
+       <a href="/">   <img src="assets/images/CWPCLogoW.png" alt="CWPC" class="top-right-image"> </a>
       </div>
       <div>
-        <a href="/home">  <img src="assets/images/arise.png" alt="arise" class="centered-image"> </a>
+        <a href="/">  <img src="assets/images/arise.png" alt="arise" class="centered-image"> </a>
       </div>
     </div>
     </a>
@@ -19,7 +19,7 @@ let generateHeader = (headerContainerElementId) =>
       </button>
       <nav class="global-navbar">
       <ul>
-          <li><a href="/home" id="home">Home</a></li>
+          <li><a href="/" id="home">Home</a></li>
           <li><a href="/using_the_scorecard" id="usescorecard">Using Scorecard</a></li>
           <li><a href="/press_release" id="press">Press Release</a></li>
           <li><a href="/scorecard_info" id="scorecardinfo">Scorecard Info</a></li>
