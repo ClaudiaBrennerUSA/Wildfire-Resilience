368c075c6f013465a9af36d65559e9d66e787aa9
sadiya-b <bhawania.s@northeastern.edu>
2024-03-27 13:08:32 -0700

    Header and about page changes

--- src/main/static_files/assets/javascript/header.js
+++ src/main/static_files/assets/javascript/header.js
@@ -2,44 +2,41 @@ let generateHeader = (headerContainerElementId) => {
   let headerContainer = document.getElementById(headerContainerElementId);
   let markup = `
   <header>
-    <div class="images-column">
-      <div>
-       <a href="/">  <img src="assets/images/cd-logo.png" alt="CDlogo" class="top-left-image"> </a>
-       <a href="/">   <img src="assets/images/CWPCLogoW.png" alt="CWPC" class="top-right-image"> </a>
-      </div>
-      <div>
+    <button id="hamburger" aria-expanded="false" onclick="toggleHamburger()" title="main menu">
+      <i class="fa-solid fa-bars" aria-hidden="true"></i> MENU
+    </button>
+    <nav class="global-navbar">
+      <div class="images-column">
+        <a href="/">  <img src="assets/images/cd-logo.png" alt="CDlogo" class="top-left-image"> </a>
+        <a href="/">  <img src="assets/images/CWPCLogoW.png" alt="CWPC" class="top-right-image"> </a>
         <a href="/">  <img src="assets/images/arise.png" alt="arise" class="centered-image"> </a>
       </div>
-    </div>
-    </a>
-    <div class="content-column">
-      <button id="hamburger" aria-expanded="false" onclick="toggleHamburger()" title="main menu">
-       <i class="fa-solid fa-bars" aria-hidden="true"></i> MENU
-      </button>
-      <nav class="global-navbar">
       <ul>
-          <li><a href="/" id="home">Home</a></li>
-          <li><a href="/using_the_scorecard" id="usescorecard">Using Scorecard</a></li>
-          <li><a href="/press_release" id="press">News</a></li>
-          <li><a href="/scorecard_info" id="scorecardinfo">Participation</a></li>
-          <li><a href="/faq" id="faq">FAQ</a></li>
-          <li><a href="/about_us" id="about">About</a></li>
-          <li><a href="/subscribe">Subscribe</a></li>
-          <li><a href="/contact_us">Contact</a></li>
-            <li style="display:none;">
-            <div class="search-box">
-              <input type="text" placeholder="Search">
-              <button type="button">Search</button>
-            </div>
-          </li>
-        </ul>
-      </nav>
-      <div class="title">
-        <h1>Community Wildfire Resilience Scorecard</h1>
-        <p>Empower Communities to Assess Wildfire Risk, Boost Preparedness</p>
+        <li><a href="/" id="home">Home</a></li>
+        <li><a href="/using_the_scorecard" id="usescorecard">Using Scorecard</a></li>
+        <li><a href="/press_release" id="press">News</a></li>
+        <li><a href="/scorecard_info" id="scorecardinfo">Participation</a></li>
+        <li><a href="/faq" id="faq">FAQ</a></li>
+        <li><a href="/about_us" id="about">About</a></li>
+        <li><a href="/subscribe">Subscribe</a></li>
+        <li><a href="/contact_us">Contact</a></li>
+        <li style="display:none;">
+          <div class="search-box">
+            <input type="text" placeholder="Search">
+            <button type="button">Search</button>
+          </div>
+        </li>
+      </ul>
+    </nav>
+    <div class="header-content">
+      <div class="content-column">
+        <div class="title">
+          <h1>Community Wildfire Resilience Scorecard</h1>
+          <p>Empower Communities to Assess Wildfire Risk, Boost Preparedness</p>
+        </div>
       </div>
     </div>
-  </header>
+    </header>
   `;
   headerContainer.innerHTML = markup;
 };
